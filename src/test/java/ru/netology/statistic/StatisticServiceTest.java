53263c06d959397d3b8b849f51f8bbaaf13f4acf
Николай <ex3ct@mail.ru>
2020-10-05 10:55:00 +0700

    updated Service

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -8,12 +8,13 @@ class StatisticServiceTest {
 
     @Test
     void findMax() {
+
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 1;
+
         StatisticService service = new StatisticService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomes);
 
         assertEquals(expected, actual);
     }
